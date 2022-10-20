a2688d2661391973703c87c4f4242831ad8ae280
Hongsugyeong <olivia258@naver.com>
2022-10-20 19:38:10 +0900

    aop

--- src/main/java/com/gang/home/aop/test/Card.java
+++ src/main/java/com/gang/home/aop/test/Card.java
@@ -4,6 +4,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +14,17 @@ import lombok.extern.slf4j.Slf4j;
 @Aspect
 public class Card {
 
+	@Before("execution(* com.gang.home.aop.test.Transport.airplane()")
+	public void before() {
+		log.info("----------Before------------");
+	}
+	
+	@After("execution(* com.gang.home.aop.test.Transport.taxi()")
+	public void after() {
+		log.info("---------After-------------");
+	}
+	
+	
 	@Around("execution(* com.gang.home.aop.test.Transport.take*())")
 	public Object cardCheck(ProceedingJoinPoint joinPoint) throws Throwable {
 		log.info("삐삐삑");
